3b38f290ab992d33247b2aafaaa71927b0baeb03
gaochong1111 <gaochong1994@sohu.com>
2019-11-20 22:58:21 +0800

    add new examples and update qmc.py PropertySolverExplicitQLTLUntil.java

--- build/distributions/run-example.sh
+++ build/distributions/run-example.sh
@@ -1,7 +1,17 @@
+INPUT_PRISM=qmc-loop/qmc-loop.prism
+INPUT_PROPS=qmc-loop/qmc-loop.props # qctl
+INPUT_PROPS=qmc-loop/qmc-loop-ltl.props # qltl
+
+INPUT_PRISM=qmc-recursive/qmc-recursive.prism
+INPUT_PROPS=qmc-recursive/qmc-recursive-ltl.props
+
+#INPUT_PRISM=qmc-example-6/qmc-example-6.prism
+#INPUT_PROPS=qmc-example-6/qmc-example-6.props
+
 # run qctl example
-# echo java -jar epmc-qmc.jar check --model-input-files qmc-loop.prism --property-input-files qmc-loop.props --model-input-type prism-qmc
-# java -jar epmc-qmc.jar check --model-input-files qmc-loop.prism --property-input-files qmc-loop.props --model-input-type prism-qmc
+#echo "java -jar epmc-qmc.jar check --model-input-files $INPUT_PRISM --property-input-files $INPUT_PROPS --model-input-type prism-qmc"
+#java -jar epmc-qmc.jar check --model-input-files $INPUT_PRISM --property-input-files $INPUT_PROPS --model-input-type prism-qmc
 
 # run qltl example
-echo java -jar epmc-qmc.jar check --model-input-files qmc-loop.prism --property-input-files qmc-ltl.props --model-input-type prism-qmc
-java -jar epmc-qmc.jar check --model-input-files qmc-loop.prism --property-input-files qmc-ltl.props --model-input-type prism-qmc
+echo "java -jar epmc-qmc.jar check --model-input-files $INPUT_PRISM  --property-input-files $INPUT_PROPS --model-input-type prism-qmc"
+java -jar epmc-qmc.jar check --model-input-files $INPUT_PRISM  --property-input-files $INPUT_PROPS --model-input-type prism-qmc
